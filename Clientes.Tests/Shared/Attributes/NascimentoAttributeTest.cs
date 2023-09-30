c286ec67d2a5e14ace44b7b56068d894691e8180
alexdamiao <alexdamiao@gmail.com>
2023-09-30 11:34:28 -0300

    test: Adicionando teste de Data de Nascimento

---
+++ Clientes.Tests/Shared/Attributes/NascimentoAttributeTest.cs
@@ -0,0 +1,36 @@
+﻿using Clientes.Api.Contracts;
+using Clientes.Api.Shared.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes.Tests.Shared.Attributes;
+
+public class NascimentoAttributeTest
+{
+    [Test]
+    public void NascimentoEhObrigatorio()
+    {
+        var request = new Mock
+        {
+            Teste = "Data errada"
+        };
+
+        var context = new ValidationContext(request, null, null);
+        var results = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(request, context, results, true);
+
+        Assert.IsFalse(isValid);
+        Assert.AreEqual(1, results.Count);
+        Assert.AreEqual("Teste está inválido.", results[0].ErrorMessage);
+    }
+
+    public class Mock 
+    {
+        [Nascimento]
+        public string Teste { get; set;}
+    }
+}
